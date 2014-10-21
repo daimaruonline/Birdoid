3fcf5aa84a4450859bcfe6a271b5dd203ea3701f
daimaruonline <dogihara@gmail.com>
2014-10-22 02:02:30 +0900

    first commit

---
+++ Assets/Scripts/Birdoid.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Birdoid : MonoBehaviour {
+
+	public Vector2 direction;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = transform.position.x + direction.x * speed * Time.deltaTime;
+		var y = transform.position.y + direction.y * speed * Time.deltaTime;
+		transform.Translate(x, y, 0);
+	}
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour
+{
+
+		public GameObject birdoid;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+				Birdoid _bird = (Birdoid)Instantiate (birdoid, birdoid.transform.position, birdoid.transform.rotation);
+				_bird.direction = new Vector2 (-0.1f, 0.1f);
+				_bird.speed = 0.05f;
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
